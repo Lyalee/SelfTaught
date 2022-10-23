8ed168f15063c67c17776ecd012eb88283abbde7
CGarnier <claudia.garnier@hotmail.com>
2022-10-23 14:25:50 -0400

    First commit

--- src/main/java/me/lyalee/FileManager.java
+++ src/main/java/me/lyalee/FileManager.java
@@ -1,6 +1,9 @@
 package me.lyalee;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
 
 public class FileManager {
     public static void createFile(String directoryName, String fileName) {
@@ -15,12 +18,35 @@ public class FileManager {
         File file = new File(directoryName + fileName);
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
-            StringBuilder fileContent;
-            while (br.readLine());
-
-        } catch (FileNotFoundException e) {
+            StringBuilder fileContent = new StringBuilder();
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                fileContent.append(currentLine);
+                //Trying to close the buffer reader so it stops reading the file so it can be deleted
+                if((currentLine=br.readLine())==null){
+                    br.close();
+                }
+            }
+            //System.out.println(fileContent);
+            return fileContent.toString();
+        } catch (IOException e) {
             System.out.println("File does not exist " + fileName);
         }
+        return "Unable to read the file";
+
+    }
+    public static void deleteFile(String directoryName, String fileName) {
+        File file = new File (directoryName + fileName);
+        if (file.exists()){
+            file.delete();
+            System.out.println("File deleted : " + fileName);
+        }
+        else{
+            System.out.println("Could not proceed to delete : " + fileName + file.getAbsolutePath());
+        }
+    }
+
+    public static void updateFile(String directoryName, String fileName) {
 
     }
 }
--- src/main/java/me/lyalee/Main.java
+++ src/main/java/me/lyalee/Main.java
@@ -2,6 +2,9 @@ package me.lyalee;
 
 public class Main {
     public static void main(String[] args) {
-        FileManager.createFile("C:\\Users\\claud\\project\\", "test2.txt");
+
+        FileManager.createFile("C:\\Users\\claud\\project\\", "test.txt");
+        FileManager.readFile("C:\\Users\\claud\\project\\", "test.txt");
+        FileManager.deleteFile("C:\\Users\\claud\\project\\", "test.txt");
     }
 }
